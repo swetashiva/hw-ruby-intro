cbced455ea3d99af01f560e9ef05d79dbf41694b
swetashiva <swetuorama@gmail.com>
2019-02-04 21:02:27 -0600

    Shivaramakrishnan_Sweta_HW1_submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,57 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+	arr.reduce(0){|sum,x| sum+x}
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+	arr.sort.last(2).reduce(0){|sum,x| sum+x} 
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+	!!arr.uniq.combination(2).detect{|a,b| a+b==n}
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+ output="Hello, " + name
+  
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+	if(s.empty?) 
+		return false
+	elsif(s[0].match(/[a-z]|[A-Z]/))
+		s[0].match(/a|e|i|o|u|A|E|I|O|U/).nil?
+	else 
+		return false
+	end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+	if(s.size!=0 && s.count('01')==s.size)
+		return (s.to_i(2)%4==0)
+	else 
+		return false
+	end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+
+	attr_accessor :isbn
+	attr_accessor :price
+
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Argument Error as either ISBN is an empty string or price is less than or equal to $0.00" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
 end
